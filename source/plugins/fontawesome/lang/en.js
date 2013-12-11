e63abcf2159dd94121588036b141409e753295e0
Richard Watt <richard@cruciblesolutions.com>
2013-12-11 17:17:13 +0000

    Initial add of files. Fix author name.

---
+++ source/plugins/fontawesome/lang/en.js
@@ -0,0 +1,9 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'fontawesome', 'en', {
+	options: 'Special Icon Options',
+	title: 'Select Icon',
+	toolbar: 'Insert Icon'
+});
